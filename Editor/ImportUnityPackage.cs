dc5a43f5bde8c91870157f6074f967a67326e531
Umut Arda Tuncar <umutarda33@gmail.com>
2023-07-26 21:03:51 +0300

    assets popup var seperated from package json

--- Editor/ImportUnityPackage.cs
+++ Editor/ImportUnityPackage.cs
@@ -6,17 +6,18 @@ namespace MultiChoiceQuestion
 {
     public static class ImportUnityPackage
     {
+        private static readonly string ASSETS_IMPORTER_JSON_PATH = Path.Combine(Application.dataPath, "../Packages/com.yayyapim.mcq/assets_importer.json");
+        
         [InitializeOnLoadMethod]
         private static void ShowImporter()
         {
-            string packageJSONPath = Path.Combine(Application.dataPath, "../Packages/com.yayyapim.mcq/package.json");
-            string packageJSONString = File.ReadAllText(packageJSONPath);
+            string packageJSONString = File.ReadAllText(ASSETS_IMPORTER_JSON_PATH);
 
             int position;
             if (!bool.Parse(new JSONVariablePattern("importer_showed", false).GetSymbolFromMatch(packageJSONString, out position)))
             {
                 ImportMCQPackage();
-                File.WriteAllText(packageJSONPath, packageJSONString.Substring(0, position) + "true" + packageJSONString.Substring(position + "false".Length));
+                File.WriteAllText(ASSETS_IMPORTER_JSON_PATH, packageJSONString.Substring(0, position) + "true" + packageJSONString.Substring(position + "false".Length));
 
             }
 
